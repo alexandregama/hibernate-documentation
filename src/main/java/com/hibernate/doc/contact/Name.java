f72ab9ed8ee5f0ceb4a0d8969c935c54c0f6d9c2
Alexandre Gama <alexandre.gama.lima+alexandre.gama.lima@gmail.com>
2016-04-27 07:42:02 -0300

    Use @Column inside Embeddable type Named

--- src/main/java/com/hibernate/doc/contact/Name.java
+++ src/main/java/com/hibernate/doc/contact/Name.java
@@ -1,12 +1,15 @@
 package com.hibernate.doc.contact;
 
+import javax.persistence.Column;
 import javax.persistence.Embeddable;
 
 @Embeddable
 public class Name {
 
+	@Column(name = "first_name") //We are able to named it field to a column on Database even using Embeddable types
 	private String firstName;
 
+	@Column(name = "last_name")
 	private String lastName;
 
 	@Deprecated //Required for Hibernate
